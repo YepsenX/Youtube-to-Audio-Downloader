52fc891762400eb5da86346048beb4a0a13e3851
YepsenX <57401455+YepsenX@users.noreply.github.com>
2022-12-11 21:52:54 -0800

    Update YT to Audio.py
    Added a few features:
    The program will now check if the number of videos in the playlist is accurate to what YouTube reports (for example, privated videos still show up for playlist video count but aren't actually accessible). If a discrepancy exists, the program will notify the user and run as intended.
    
    Discovered an issue where certain videos that remained on the list were not able to be downloaded. The program will now skip these & provide the urls for skipped videos after the program has finished running.

--- YT to Audio.py
+++ YT to Audio.py
@@ -4,16 +4,24 @@ from pytube import Playlist
 my_playlist = input('URL of Playlist: ')
 # convert url to playlist object
 my_playlist = Playlist(my_playlist)
-save_path = input('Folder Path of Install Folder: ')
+save_path = input('Install Folder Path: ')
 total_videos = my_playlist.length
+actual_length = 0
 url_list = []
+unavailable_videos = []
 
-# creates a list of all videos in playlist
-print('\n')
-x = 0
-while x < total_videos:
-    url_list.append(my_playlist.video_urls[x])
-    x += 1
+# Create a list containing URLs of every video in the playlist
+try:
+    print('\nChecking videos in playlist...')
+    x = 0
+    for i in range(total_videos):
+        url_list.append(my_playlist.video_urls[x])
+        actual_length += 1
+        x += 1
+# This error will occur if there are privated / removed videos present in the playlist
+except IndexError or StopIteration:
+    print(f'\nWarning: removed videos exist in playlist.\n{actual_length} of {total_videos} available.')
+    total_videos = actual_length
 
 
 # Creates a list & function to remove bad characters from file names
@@ -46,15 +54,28 @@ def get_file_type(video):
 
 # Download Videos
 while len(url_list) > 0:
-    # convert list item to an object
-    video = YouTube(url_list[0])
-    # create file name
-    file_name = get_file_type(video)
-    # download the video
-    print(f'Starting Download: {file_name}')
-    video.streams.filter(type='audio').order_by('abr').last().download(save_path, file_name)
-    url_list.remove(url_list[0])
+    try:
+        # convert list item to an object
+        video = YouTube(url_list[0])
+        # create file name
+        file_name = get_file_type(video)
+        # download the video
+        print(f'Downloading: {file_name}')
+        video.streams.filter(type='audio').order_by('abr').last().download(save_path, file_name)
+        url_list.remove(url_list[0])
+    # If a video is unable to download, it will be skipped, then reported when the program is finished
+    except KeyError:
+        print(f'Error: A video is unable to download')
+        unavailable_videos.append(url_list[0])
+        url_list.remove(url_list[0])
+
 
 print('\n--------------------------')
 print('| All Downloads Complete |')
 print('--------------------------')
+
+# notifies user of any videos that were unable to download
+if len(unavailable_videos) > 0:
+    print('\nSome videos were not able to be downloaded.\nVideos in question:')
+    for i in unavailable_videos:
+        print(i)
